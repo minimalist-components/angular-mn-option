1e038c401a8cb07671258a8e3e1fef63e1d3ee60
Darlan Mendonça <darlanmendonca@gmail.com>
2017-03-10 20:13:48 -0300

    update mn-option

--- sources/angular/mn-option/mn-option.directive.js
+++ sources/angular/mn-option/mn-option.directive.js
@@ -4,27 +4,38 @@ angular
 
 function mnOptionDirective($parse) {
   return {
+
+  }
+
+  const obj = {
     restrict: 'E',
+    controller,
     link,
     require: 'ngModel',
   }
+  console.log(obj)
+
+  function controller($scope, $element, $attrs) {
+    const name = $element.attr('name')
+      || $attrs.ngModel.split('.')[$attrs.ngModel.split('.').length - 1]
+
+    $element.attr('name', name)
+    $element[0].removeAttribute('checked')
+  }
 
   function link(scope, element, attributes) {
-    const name = element.attr('name')
-      || attributes.ngModel.split('.')[attributes.ngModel.split('.').length - 1]
     const modelValue = $parse(attributes.ngModel)(scope)
     const modelApplied = angular.equals(element[0].value, modelValue)
 
-    element.attr('name', name)
-
     if (!modelApplied) {
       element[0].value = modelValue
+      // console.log(modelValue)
       $parse(attributes.ngModel).assign(scope, element[0].value)
     }
 
-    element.find('input').on('change', () => {
-      const option = event.target.closest('mn-option')
-      $parse(attributes.ngModel).assign(scope, option.value)
+    element.find('input').on('change', (event) => {
+      console.log(event.target)
+      $parse(attributes.ngModel).assign(scope, element[0].value)
       scope.$apply()
     })
 
