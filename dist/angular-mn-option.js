d4b616ef7a639382025f21c1de6d077c60763ff2
Darlan Mendonça <darlanmendonca@gmail.com>
2017-03-10 14:58:09 -0300

    add dist and docs

---
+++ dist/angular-mn-option.js
@@ -0,0 +1,2 @@
+"use strict";function mnOptionDirective($compile,$parse){function link(){}return{restrict:"E",link:link,require:"ngModel"}}angular.module("mn-option",[]),mnOptionDirective.$inject=["$compile","$parse"],angular.module("mn-option").directive("mnOption",mnOptionDirective);
+//# sourceMappingURL=angular-mn-option.js.map
